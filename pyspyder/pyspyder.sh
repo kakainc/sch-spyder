241607e2c6788bbc072348cc7515eeeb16705b83
liumeiyu2014 <liumeiyu2014@xiaochuankeji.cn>
2021-09-02 22:17:12 +0800

    spyders

---
+++ pyspyder/pyspyder.sh
@@ -0,0 +1,8 @@
+pyspider
+
+# 1.modify async
+# 2.download phantomjs (http://phantomjs.org/download.html)
+# 3.flask         1.0.2
+# 4.wsgidav       2.4.1
+# 5.werkzeug      0.16.1
+# 6.lsof -i:25555  kill -9
