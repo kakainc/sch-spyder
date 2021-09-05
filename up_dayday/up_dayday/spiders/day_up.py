b322839c46397498d9eb1c77127011c5c7f2a85f
kaka <13101385022@163.com>
2021-09-05 23:46:46 +0800

    fof

--- up_dayday/up_dayday/spiders/day_up.py
+++ up_dayday/up_dayday/spiders/day_up.py
@@ -8,17 +8,21 @@ class DaySpider(scrapy.Spider):
     def parse(self, response):
         if response.url in self.start_urls:
             urls = response.xpath('//*[@id="tblite_fof"]/tbody/tr[1]/td[2]/a/@href').extract()
+            print(urls)
             name = response.xpath('//*[@id="tblite_fof"]/tbody/tr[1]/td[2]/a/text()').extract()
+            print(name)
             for url in urls:
                 yield scrapy.Request(url, callback=self.parse)
         else:
             self.parse_detail(response)
 
     def parse_detail(self, response):
-        name = response.xpath('//*[@id="position_shares"]/div[1]/table/tbody/tr[2]/td[1]/a/text()').extract()
-
-        scale = response.xpath(
-            '//*[@id="body"]/div[4]/div[9]/div/div/div[2]/div[1]/div[3]/table/tbody/tr[1]/td[2]/text()').extract()
-        ttt = response.xpath('//*[@id="highcharts-24"]/svg/g[5]/g[3]/rect[4]/@height').extract()
-        # print (response.url, name, nh_shouyi, start_date, scale, ttt)
-        print('{};;{};;{};;{};;{};;{}'.format(name[0], response.url, nh_shouyi[0], start_date[0], scale[0], ttt[0]))
+        name = response.xpath('//*[@id="body"]/div[11]/div/div/div[1]/div[1]/div/text()').extract()[0]
+        num = response.xpath('//*[@id="body"]/div[11]/div/div/div[1]/div[1]/div/span[2]/text()').extract()[0]
+        # parts = []
+        part = response.xpath(
+            '/html/body/div[2]/div[13]/div/div/div[1]/div[2]/div[2]/ul/li[1]/div[1]/table/tbody/tr[2]/td[1]/a/@href').extract()
+        # for i in range(2, 12):
+        #     part = response.xpath('//*[@id="position_shares"]/div[1]/table/tbody/tr[2]/td[1]/a/text()').extract()
+        #     parts.append(part)
+        print(response.url, name, num, part)
