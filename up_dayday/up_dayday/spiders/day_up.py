167e07b1e288c62f51d546ecc375bd0ce73f9d21
liumeiyu2014 <liumeiyu2014@xiaochuankeji.cn>
2021-09-17 21:37:41 +0800

    update

--- up_dayday/up_dayday/spiders/day_up.py
+++ up_dayday/up_dayday/spiders/day_up.py
@@ -8,30 +8,35 @@ class DaySpider(scrapy.Spider):
     def parse(self, response):
         if response.url in self.start_urls:
             urls = response.xpath('//*[@id="tblite_fof"]/tbody/tr[1]/td[2]/a/@href').extract()
-            print(urls)
             name = response.xpath('//*[@id="tblite_fof"]/tbody/tr[1]/td[2]/a/text()').extract()
-            print(name)
             for url in urls:
                 yield scrapy.Request(url, callback=self.parse)
         else:
             self.parse_detail(response)
 
-    def parse_detail(self, response):
-        name = response.xpath('//*[@id="body"]/div[11]/div/div/div[1]/div[1]/div/text()').extract()[0]
-        num = response.xpath('//*[@id="body"]/div[11]/div/div/div[1]/div[1]/div/span[2]/text()').extract()[0]
-        one_month = \
-            response.xpath('//*[@id="body"]/div[11]/div/div/div[2]/div[1]/div[1]/dl[1]/dd[2]/span[2]/text()').extract()
-        three_month = response.xpath('//*[@id="body"]/div[11]/div/div/div[2]/div[1]/div[1]/dl[1]/dd[2]/span[2]/text()')
-        six_month = \
-            response.xpath('//*[@id="body"]/div[11]/div/div/div[2]/div[1]/div[1]/dl[1]/dd[2]/span[2]/text()').extract()
-        one_year = \
-            response.xpath('//*[@id="body"]/div[11]/div/div/div[2]/div[1]/div[1]/dl[1]/dd[2]/span[2]/text()').extract()
-
-
+    def parse_detail(self, response, parse_eve_detail=False):
+        name = response.xpath('//*[@id="body"]/div[11]/div/div/div[1]/div[1]/div/text()').extract_first()
+        num = response.xpath('//*[@id="body"]/div[11]/div/div/div[1]/div[1]/div/span[2]/text()').extract_first()
+        one_month = response.xpath(
+            '//*[@id="body"]/div[11]/div/div/div[2]/div[1]/div[1]/dl[1]/dd[2]/span[2]/text()').extract_first()
+        three_month = response.xpath(
+            '//*[@id="body"]/div[11]/div/div/div[2]/div[1]/div[1]/dl[1]/dd[2]/span[2]/text()').extract_first()
+        six_month = response.xpath(
+            '//*[@id="body"]/div[11]/div/div/div[2]/div[1]/div[1]/dl[1]/dd[2]/span[2]/text()').extract_first()
+        one_year = response.xpath(
+            '//*[@id="body"]/div[11]/div/div/div[2]/div[1]/div[1]/dl[1]/dd[2]/span[2]/text()').extract_first()
 
         parts = {}
         for i in range(2, 12):
-            part = response.xpath('//*[@id="position_shares"]/div[1]/table/tr[{}]/td[1]/a/text()'.format(i)).extract()[0]
-            url = response.xpath('//*[@id="position_shares"]/div[1]/table/tr[{}]/td[1]/a/@href'.format(i)).extract()[0]
-            parts[part] = url
-        print(response.url, name, num, parts)
+            part = response.xpath(
+                '//*[@id="position_shares"]/div[1]/table/tr[{}]/td[1]/a/text()'.format(i)).extract_first()
+            url_page = response.xpath(
+                '//*[@id="position_shares"]/div[1]/table/tr[{}]/td[1]/a/@href'.format(i)).extract_first()
+            if url_page is not None and parse_eve_detail:
+                yield scrapy.Request(url_page, self.eve_parse)
+            else:
+                parts[part] = url_page
+        print(response.url, name, num, parts, one_month)
+
+    def eve_parse(self):
+        pass
